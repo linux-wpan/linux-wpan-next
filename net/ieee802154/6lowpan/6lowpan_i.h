5898f59c4c4d6c76835c88ddedf286d5c9fd97f7
Alexander Aring <alex.aring@gmail.com>
2014-08-23 15:54:41 +0200

    ieee802154: 6lowpan: split tx part into tx.c

--- net/ieee802154/6lowpan/6lowpan_i.h
+++ net/ieee802154/6lowpan/6lowpan_i.h
@@ -22,6 +22,10 @@ lowpan_dev_info *lowpan_dev_info(const struct net_device *dev)
 	return netdev_priv(dev);
 }
 
+int lowpan_header_create(struct sk_buff *skb, struct net_device *dev,
+			 unsigned short type, const void *_daddr,
+			 const void *_saddr, unsigned int len);
+netdev_tx_t lowpan_xmit(struct sk_buff *skb, struct net_device *dev);
 void lowpan_init_rx(void);
 void lowpan_cleanup_rx(void);
 
