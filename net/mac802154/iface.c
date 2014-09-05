f29addc671f199d1ed04d4119dd85fa6c72424fa
Alexander Aring <alex.aring@gmail.com>
2014-09-05 12:50:54 +0200

    mac802154: increase tx queue like ethernet to 1000

--- net/mac802154/iface.c
+++ net/mac802154/iface.c
@@ -337,7 +337,7 @@ static void ieee802154_if_setup(struct net_device *dev)
 	dev->header_ops		= &mac802154_header_ops;
 	dev->needed_tailroom	= 2 + 16; /* FCS + MIC */
 	dev->mtu		= IEEE802154_MTU;
-	dev->tx_queue_len	= 300;
+	dev->tx_queue_len	= 1000;
 	dev->type		= ARPHRD_IEEE802154;
 	dev->flags		= IFF_NOARP | IFF_BROADCAST;
 	dev->watchdog_timeo	= 0;
