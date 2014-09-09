8b120bb4cd1f01df1be38da21102b2b15def14b0
Alexander Aring <alex.aring@gmail.com>
2014-09-09 18:44:04 +0200

    mac802154: simplify ioctl to set/get short addr
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>

--- net/mac802154/iface.c
+++ net/mac802154/iface.c
@@ -137,53 +137,39 @@ mac802154_wpan_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 	struct wpan_dev *wpan_dev = &sdata->wpan_dev;
 	struct sockaddr_ieee802154 *sa =
 		(struct sockaddr_ieee802154 *)&ifr->ifr_addr;
-	int err = -ENOIOCTLCMD;
 
 	ASSERT_RTNL();
 
+	/* TODO what's about extended? */
+
 	switch (cmd) {
 	case SIOCGIFADDR:
-	{
-		u16 pan_id, short_addr;
-
-		pan_id = le16_to_cpu(wpan_dev->pan_id);
-		short_addr = le16_to_cpu(wpan_dev->short_addr);
-		if (pan_id == IEEE802154_PANID_BROADCAST ||
-		    short_addr == IEEE802154_ADDR_BROADCAST) {
-			err = -EADDRNOTAVAIL;
-			break;
-		}
-
 		sa->family = AF_IEEE802154;
 		sa->addr.addr_type = IEEE802154_ADDR_SHORT;
-		sa->addr.pan_id = pan_id;
-		sa->addr.short_addr = short_addr;
-
-		err = 0;
+		sa->addr.pan_id = le16_to_cpu(wpan_dev->pan_id);
+		sa->addr.short_addr = le16_to_cpu(wpan_dev->short_addr);
 		break;
-	}
 	case SIOCSIFADDR:
 		if (netif_running(dev))
 			return -EBUSY;
 
 		dev_warn(&dev->dev,
-			 "Using DEBUGing ioctl SIOCSIFADDR isn't recommened!\n");
+			 "Using ioctl SIOCSIFADDR isn't recommened!\n");
 		if (sa->family != AF_IEEE802154 ||
 		    sa->addr.addr_type != IEEE802154_ADDR_SHORT ||
 		    sa->addr.pan_id == IEEE802154_PANID_BROADCAST ||
 		    sa->addr.short_addr == IEEE802154_ADDR_BROADCAST ||
-		    sa->addr.short_addr == IEEE802154_ADDR_UNDEF) {
-			err = -EINVAL;
-			break;
-		}
+		    sa->addr.short_addr == IEEE802154_ADDR_UNDEF)
+			return -EINVAL;
 
 		wpan_dev->pan_id = cpu_to_le16(sa->addr.pan_id);
 		wpan_dev->short_addr = cpu_to_le16(sa->addr.short_addr);
-
 		break;
+	default:
+		return -ENOIOCTLCMD;
 	}
 
-	return err;
+	return 0;
 }
 
 static int mac802154_wpan_mac_addr(struct net_device *dev, void *p)
